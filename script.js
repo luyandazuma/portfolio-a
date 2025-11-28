ab4d2f410df4e5542798807391e2fa80eea49992
Luyanda <luyanda.s.zuma@gmail.com>
2025-11-28 10:24:52 +0200

    added navigation script

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ function toggleTheme() {
     body.classList.add("dark-mode");
   }
 }
+
+// navigation
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const targetId = link.getAttribute("data-section");
+    scrollToSection(targetId);
+
+    // Update active state
+    navLinks.forEach((l) => l.classList.remove("active"));
+    link.classList.add("active");
+  });
+});
+
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: "smooth" });
+}
