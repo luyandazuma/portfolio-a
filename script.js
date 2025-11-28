05a80490958f8fa66cf95c660224a8ff6c65086c
Luyanda <luyanda.s.zuma@gmail.com>
2025-11-28 10:29:13 +0200

    added typewriter effect script

--- script.js
+++ script.js
@@ -66,3 +66,24 @@ navLinks.forEach((link) => {
 sections.forEach((section) => {
   observer.observe(section);
 });
+
+// Download CV
+function downloadCV() {}
+
+// typing effect for typewriter
+const typingText = document.querySelector(".typing-text");
+if (typingText) {
+  const text = typingText.textContent;
+  typingText.textContent = "";
+  let i = 0;
+
+  function typeWriter() {
+    if (i < text.length) {
+      typingText.textContent += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100);
+    }
+  }
+
+  setTimeout(typeWriter, 500);
+}
